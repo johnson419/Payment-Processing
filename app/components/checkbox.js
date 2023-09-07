8f2e10a52a7e97451b8023e7cf78c4d7910c0983
Johnson Fredrick <johnsonfredrick577@gmail.com>
2023-09-07 13:01:30 +0300

    folder organization

---
+++ app/components/checkbox.js
@@ -0,0 +1,22 @@
+import React from "react";
+ function Checkbox() {
+  return (
+    <div>
+      <label>
+        <input type="checkbox" class="checked:bg-blue-500" checked=""></input>
+        <span className="ml-2 text-gray-800">M-pesa</span>
+      </label>
+
+      <label className="flex items-center">
+        <input type="checkbox" class="form-checkbox text-indigo-600"></input>
+        <span className="ml-2 text-gray-800">Tigopesa</span>
+      </label>
+      <label className="flex items-center">
+        <input type="checkbox" class="form-checkbox text-indigo-600"></input>
+        <span className="ml-2 text-gray-800">Airtel Money</span>
+      </label>
+    </div>
+  );
+}
+
+export default Checkbox;
--- app/page.js
+++ app/page.js
@@ -1,26 +1,16 @@
+import React from "react";
+import { Checkbox } from "./components/checkbox";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-12">
       <div>
         <h1 class="text-4xl font-bold">Payment App</h1>
         <div>
-          <p className="flex min-h-screen flex-col items-center justify-between text-2xl pt-4">Choose Payment Vendor</p>
-          <p>hello</p>
-          
-  <label>
-      <input type="checkbox" checked></input> 
-  Browser default
-  </label>
-  <label className="flex items-center">
-    <input type="checkbox" class="form-checkbox text-indigo-600"></input>
-    <span className="ml-2 text-gray-800">Tigopesa</span>
-  </label>
-  <label className="flex items-center">
-    <input type="checkbox" class="form-checkbox text-indigo-600"></input>
-    <span className="ml-2 text-gray-800">Airtel Money</span>
-  </label>
+          <p className="flex flex-col items-center justify-between text-2xl pt-4">
+            Choose Payment Vendor
+          </p>
 
-
+          <Checkbox />
         </div>
       </div>
     </main>
