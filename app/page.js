c754b3b2500bc779d7a54c2fa743040de69ae250
Johnson Fredrick <johnsonfredrick577@gmail.com>
2023-09-07 11:19:01 +0300

    checkbox implementation

--- app/page.js
+++ app/page.js
@@ -2,10 +2,27 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
-        <h1 className="text-4xl font-bold">Payment App</h1>
+        <h1 class="text-4xl font-bold">Payment App</h1>
         <div>
           <p className="flex min-h-screen flex-col items-center justify-between text-2xl pt-4">Choose Payment Vendor</p>
-          
+          <p>hello</p>
+          <div className="space-y-2">
+  <label className="flex items-center">
+    
+  <input type="checkbox" checked></input> 
+  Browser default
+
+  </label>
+  <label className="flex items-center">
+    <input type="checkbox" class="form-checkbox text-indigo-600"></input>
+    <span className="ml-2 text-gray-800">Tigopesa</span>
+  </label>
+  <label className="flex items-center">
+    <input type="checkbox" class="form-checkbox text-indigo-600"></input>
+    <span className="ml-2 text-gray-800">Airtel Money</span>
+  </label>
+</div>
+
         </div>
       </div>
     </main>
